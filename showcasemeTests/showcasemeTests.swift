982de86172419477ed84a006d7359579d644868d
PatelShakil <patelsakib95@gmail.com>
2025-03-18 13:49:10 +0530

    Initial Commit

---
+++ showcasemeTests/showcasemeTests.swift
@@ -0,0 +1,17 @@
+//
+//  showcasemeTests.swift
+//  showcasemeTests
+//
+//  Created by ict2batch1 on 18/03/25.
+//
+
+import Testing
+@testable import showcaseme
+
+struct showcasemeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ showcasemeUITests/showcasemeUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  showcasemeUITestsLaunchTests.swift
+//  showcasemeUITests
+//
+//  Created by ict2batch1 on 18/03/25.
+//
+
+import XCTest
+
+final class showcasemeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
