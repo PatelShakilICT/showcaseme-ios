13f6749c178616a462fb7a0a3f08dc4f74605a77
PatelShakil <patelsakib95@gmail.com>
2025-03-19 11:44:26 +0530

    Web View Final

--- showcaseme/ViewController.swift
+++ showcaseme/ViewController.swift
@@ -1,19 +1,70 @@
-//
-//  ViewController.swift
-//  showcaseme
-//
-//  Created by ict2batch1 on 18/03/25.
-//
-
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var webView: WKWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create WKWebViewConfiguration
+        let webConfiguration = WKWebViewConfiguration()
+        webConfiguration.dataDetectorTypes = [.all]
+
+        // Initialize WKWebView
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(webView)
+        
+        // Set constraints for webView
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        // Assign navigation delegate before loading URL
+        webView.navigationDelegate = self
+        
+        var url = "http://192.168.61.51:5173/dashboard"
+        
+        // Load URL
+        if let myURL = URL(string: url) {
+            let myRequest = URLRequest(url: myURL)
+            webView.load(myRequest)
+        }
+
+        // Handle content inset for iOS 11+
+        if #available(iOS 11.0, *) {
+            webView.scrollView.contentInsetAdjustmentBehavior = .never
+        }
+        
+        // Enable back/forward gestures
+        webView.allowsBackForwardNavigationGestures = true
     }
-
-
 }
 
+extension ViewController: WKNavigationDelegate {
+    @objc func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction,
+                       decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        guard let url = navigationAction.request.url else {
+            decisionHandler(.cancel)
+            return
+        }
+
+        let string = url.absoluteString
+        if string.contains("mailto:") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            decisionHandler(.cancel)
+            return
+        }
+        if string.contains("sms:") {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            decisionHandler(.cancel)
+            return
+        }
+        decisionHandler(.allow)
+    }
+}
